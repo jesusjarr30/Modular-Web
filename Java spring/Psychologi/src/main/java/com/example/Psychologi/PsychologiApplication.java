5e458f4f104b53e99ee1395c8e6df74fc8f54f38
Jesus Rodriguez <jesus.jarr.30@gmail.com>
2023-06-18 22:37:04 -0600

    Java spring boot code

---
+++ Java spring/Psychologi/src/main/java/com/example/Psychologi/PsychologiApplication.java
@@ -0,0 +1,13 @@
+package com.example.Psychologi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsychologiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsychologiApplication.class, args);
+	}
+
+}
---
+++ Java spring/Psychologi/src/test/java/com/example/Psychologi/PsychologiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Psychologi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsychologiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

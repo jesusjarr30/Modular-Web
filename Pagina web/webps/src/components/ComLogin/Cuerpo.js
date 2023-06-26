8716796936d56e696a5108fb788f5e8775fc7e57
Jesus Rodriguez <jesus.jarr.30@gmail.com>
2023-06-26 00:34:24 -0600

    Redefine login

--- Pagina web/webps/src/components/ComLogin/Cuerpo.js
+++ Pagina web/webps/src/components/ComLogin/Cuerpo.js
@@ -1,6 +1,6 @@
-import React from 'react'
-import React, { useEffect } from 'react';
+import { useState, useEffect } from "react";
 import axios from 'axios';
+import cerebro from '../Imagenes/cerebro.png';
 
 ///fañta importar los recursos que se necesitan
 function Cuerpo () {
@@ -11,11 +11,10 @@ function Cuerpo () {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get('http:');
+        const response = await axios.get('http:\\localhost');
         console.log(response.data); // Process the response data here
       } catch (error) {
         console.error(error);
-
       }
     };
 
@@ -24,7 +23,7 @@ function Cuerpo () {
 
 
   return (
-    <div className="hero min-h-screen bg-base-200">
+    <div className="hero2 min-h-screen bg-base-200">
       <div className="hero-content flex-col lg:flex-row-reverse">
         <div className="text-center lg:text-left">
           <h1 className="text-5xl font-bold">Login now!</h1>
@@ -53,7 +52,23 @@ function Cuerpo () {
           </div>
         </div>
       </div>
+      <footer className="footer p-10 bg-neutral text-neutral-content">
+            <div>
+            <div className="iconos">
+                <a href="#"><img src={cerebro} alt="" /></a>
+              </div>
+              <p>Copyright © 2023  <br/>All right reserved by ACME Industries Ltd</p>
+            </div> 
+            <div>
+                <span className="footer-title">Social</span> 
+                <div className="grid grid-flow-col gap-4">
+                <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path></svg></a> 
+                <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path></svg></a>
+                </div>
+            </div>
+        </footer>
     </div>
+    
   )
 }
 export default Cuerpo;
--- Pagina web/webps/src/components/Login.js
+++ Pagina web/webps/src/components/Login.js
@@ -2,11 +2,12 @@ import React from "react";
 import Cuerpo from "./ComLogin/Cuerpo";
 import Footer from "./ComLogin/Footer";
 
+
 function Login(){
 return (
     <>
     <Cuerpo/>
-    <Footer/>
+    
     </>
 )
 }

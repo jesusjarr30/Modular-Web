fc3d053de24824ccaf5c5e346c32eb47a1f77e50
JesusRRenteria <126137256+JesusRRenteria@users.noreply.github.com>
2023-06-26 18:48:33 -0600

    test login

--- Pagina web/webps/src/components/ComLogin/Cuerpo.js
+++ Pagina web/webps/src/components/ComLogin/Cuerpo.js
@@ -5,28 +5,21 @@ import cerebro from '../Imagenes/cerebro.png';
 ///fañta importar los recursos que se necesitan
 function Cuerpo () {
 
-  const [username, setUsername] = useState('');
+  const [correo, setCorreo] = useState('');
   const [password, setPassword] = useState('');
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get('http:\\localhost');
-        console.log(response.data); // Process the response data here
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    fetchData();
-  }, []);
+  
+  function verDatos(){
+    console.log('El correo es ',correo)
+    console.log('la contraseña es',password)
+  }
 
 
   return (
     <div className="hero2 min-h-screen bg-base-200">
       <div className="hero-content flex-col lg:flex-row-reverse">
         <div className="text-center lg:text-left">
-          <h1 className="text-5xl font-bold">Login now!</h1>
+          <h1 className="text-5xl font-bold">Inicio de sección!</h1>
           <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
         </div>
         <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
@@ -35,19 +28,19 @@ function Cuerpo () {
               <label className="label">
                 <span className="label-text">Email</span>
               </label>
-              <input type="text" placeholder="email" className="input input-bordered" />
+              <input type="text" placeholder="email" className="input input-bordered" value={correo} onChange={(e) => setCorreo(e.target.value)}/>
             </div>
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Password</span>
               </label>
-              <input type="text" placeholder="password" className="input input-bordered" />
+              <input type="text" placeholder="password" className="input input-bordered" value={password} onChange={(e) => setPassword(e.target.value)}/>
               <label className="label">
                 <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
               </label>
             </div>
             <div className="form-control mt-6">
-              <button className="btn btn-primary">Login</button>
+              <button onClick={verDatos}className="btn btn-primary">Ingresar</button>
             </div>
           </div>
         </div>

345012468ecdb98a9a173a4b8b2f64708722a590
jesusjarr30 <jesus.jarr.30@gmail.com>
2023-08-17 23:39:00 -0600

    list with pagination

--- PaginaWeb/webps/src/components/Dashboard/Userlist.js
+++ PaginaWeb/webps/src/components/Dashboard/Userlist.js
@@ -5,6 +5,7 @@ import DataTable from 'react-data-table-component';
 
 const Userlist = ({ nombre, id }) => {
 const [customerList, setCustomerList] = useState([]);
+const [busqueda,setBusqueda] = useState([])
 
   useEffect(() => {
     const fetchData = async () => {
@@ -13,6 +14,7 @@ const [customerList, setCustomerList] = useState([]);
         console.log(response);
         console.log("Esta es la informacion que se debe de imprimir")
         setCustomerList(response.data);
+        setBusqueda(response.data)
       } catch (error) {
         console.error("Error fetching data:", error);
       }
@@ -25,18 +27,22 @@ const [customerList, setCustomerList] = useState([]);
     {
         name: 'Nombre',
         selector: row => row.name,
+        sortable: true
     },
     {
       name:'Correo',
       selector: row => row.email,
+      sortable: true
     },
     {
         name: 'Year',
         selector: row => row.year,
+        sortable: true
     },
     {
       name: 'Telefono',
-      selector: row => row.telephone
+      selector: row => row.telephone,
+      sortable: true
     }
     
 ];
@@ -44,15 +50,34 @@ const [customerList, setCustomerList] = useState([]);
   
 
 
+  function handlerFilter(event) {
+    const newData = customerList.filter(row => {
+      return row.name.toLowerCase().includes(event.target.value.toLowerCase())
+    })
+    setBusqueda(newData)
+  }
+
   return (
     <div className="flex w-full h-screen justify-center ">
-      <h1>Aqui va la lista de usuarios:</h1>
+      
       <div className="w-32 md:w-11/12 bg-white px-8 py-10 rounded-3xl border-2 border-gray-200 mt-8 p-6">
+      <h1>Aqui va la lista de usuarios:</h1>
+      <div className="text-end">
+        <input type="text" onChange={handlerFilter}>
+
+        </input>
+
+      </div>
+      <div className="container">
         <DataTable 
           columns={columns}
-          data={customerList}
+          data={busqueda}
+          selectableRows
+          fixedHeader
+          pagination
         
         />
+        </div>
       </div>
     </div>
   );

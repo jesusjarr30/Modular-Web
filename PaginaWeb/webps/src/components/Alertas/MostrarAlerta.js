13b9a1fe509075cbac5570a3de63eabf1009d5ad
jesusjarr30 <jesus.jarr.30@gmail.com>
2023-09-04 17:44:46 -0600

    Edit enable

---
+++ PaginaWeb/webps/src/components/Alertas/MostrarAlerta.js
@@ -0,0 +1,64 @@
+import Swal from 'sweetalert2';
+
+export const MostrarAlerta = (tipo, opciones = {}) => {
+  //aqui van todas las alertas peronalizadas
+
+  const defaultOptions = {
+    title: 'Alerta', // Título predeterminado
+    icon: 'info',    // Icono predeterminado
+    text: '',         // Texto predeterminado
+    timer: null,      // Temporizador predeterminado (sin límite de tiempo)
+    customClass: {
+      footer: 'swal-footer',
+
+    },
+    ...opciones,      // Fusiona las opciones personalizadas con las predeterminadas
+  };
+
+  const options = {
+    ...defaultOptions,
+    ...opciones, // Sobreescritura de opciones personalizadas
+  };
+
+  if (tipo === 'Servidor') {
+    Swal.fire({
+      ...options,
+      icon: 'error',
+      text: 'El correo ya está registrado con otra cuenta',
+      footer: '<a href="">Olvidó su contraseña?</a>',
+      timer: 3000,
+    });
+  } else if (tipo === 'Exito') {
+    Swal.fire({
+      
+      icon: 'success',
+      text: 'Registrado con éxito!',
+    }).then(() => {
+      // Después de mostrar el SweetAlert de éxito, regresar a la página previa
+      window.history.back();
+    });
+  } else if (tipo === 'Error') {
+    Swal.fire({
+      ...options,
+      icon: 'info',
+      text: 'El correo ya está registrado con otra cuenta',
+      timer: 5000,
+    });
+  }
+
+  //este es un ejemplo de como se deberia de ver alertas
+  if(tipo === "Servidor"){    
+    Swal.fire({
+      title: 'Error',
+      icon: 'error',//los iconos despinibles para este tipo de tareas son las siguientes: error, succesful, warning, info
+      //utilizar los warning para la generacionde expectios.
+
+      text: 'El correo ya esta registrado con otra cuenta',
+      footer: '<a href="">Olvido su contraseña?</a>',
+      timer:3000,
+      customClass: {
+        footer: 'swal-footer',
+      }
+    })
+
+};
--- PaginaWeb/webps/src/components/Dashboard/Personalizar.js
+++ PaginaWeb/webps/src/components/Dashboard/Personalizar.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect} from "react";
 import axios from 'axios';
 import Swal from 'sweetalert2';
 import { Button } from "react-bootstrap";
+//import { MostrarAlerta } from "../Alertas/MostrarAlerta";
 
 const Perzonalizar = ({ nombre, id}) => {
 
@@ -15,41 +16,37 @@ const Perzonalizar = ({ nombre, id}) => {
     const [Password,setPassword] = useState('');
     const[Confirmation,setConfirmation] = useState('');
     const[Current,setCurrent] = useState('');
-  //Estos estados se usan para la confirmacion
+ //variable para modificar los los estados del boton
+  const [editable, setEditable] = useState(false);
 
+  const toggleEditable = () => {
+    setEditable(!editable);
+  };
+ //Estos estados se usan para la confirmacion
   const [data,setData] = useState([]);
 
-    const ModificarClick = () => {
+  const ModificarClick = () => {
+    const newData = {}; // Crear un objeto vacío para almacenar las modificaciones
+  
+    if (Psicologo.nombre !== Name) {
+      console.log("El nombre fue modificado");
+      newData.nombre = Name; // Agregar el nombre al objeto newData
+    }
+    if (Psicologo.apellidos !== Apellidos) {
+      newData.apellidos = Apellidos; // Agregar los apellidos al objeto newData
+    }
+    if (Psicologo.telephone !== Telephone) {
+      console.log("El teléfono fue modificado");
+      newData.telephone = Telephone; // Agregar el teléfono al objeto newData
+    }
+    if (Psicologo.password === Password && Current === Confirmation) {
+      console.log("Las contraseñas ingresadas son iguales");
+      newData.password = Password; // Agregar la contraseña al objeto newData
+    }
+    // Enviar solo un objeto JSON con todas las modificaciones al servidor
+    enviarSolicitudAlServidor(newData);
+  };
 
-      //tenemos que hacer todas las las validaciones para agregar a http los requisitos que se necesitan
-      //validate name
-      if(Psicologo.nombre != Name){
-        console.log("EL nombre fue modificado");
-        const nuevaTupla ={nombre: Name}
-        setData([...data,nuevaTupla])
-
-        //poner aqui las validaciones necesarias
-      }
-      if(Psicologo.apellidos != Apellidos){
-        console.log("Se intenta modificar los apellidos");
-        console.log("EL nombre fue modificado");
-        const nuevaTupla ={apellidos : Apellidos}
-        setData([...data,nuevaTupla])
-
-      }
-      if(Psicologo.telephone != Telephone){
-        console.log("EL telefono fue modificado");
-      }
-      if(Psicologo.password === Password){
-        console.log("la contraeña es la misma por loq ue se puede proceder al cambio");
-
-        if(Current === Confirmation){
-          console.log("Las contraseñas que ingreso son iguales");
-        }
-      }
-      
-      
-    };
     {/* Funcion para obtener todos los datos de psicologo*/ }
     useEffect(() => {
         const fetchData = async () => {
@@ -77,98 +74,94 @@ const Perzonalizar = ({ nombre, id}) => {
       };
     
 {/**Metodo para poder hacer la modificaciones necesarias la objeto */}
-const updateData = () => {
-  const url = "http://localhost:8080/UpdatePsy/a6855fbe-12d2-43d6-9b20-bdba035e20f6";
-  const data = {
-    nombre: "jose"
-  };
+const enviarSolicitudAlServidor = (newData) => {
+ 
 
-  axios.put(url, data, {
+  const url = "http://localhost:8080/UpdatePsy/"+id;
+
+  // Establecer un tiempo de espera de 10 segundos (10000 milisegundos)
+  const timeoutMillis = 5000;
+
+  axios.put(url, newData, {
     headers: {
       "Accept": "*/*",
       "Content-Type": "application/json"
-    }
+    },
+    timeout: timeoutMillis // Establecer el tiempo de espera
   })
   .then(response => {
     // Manejar la respuesta aquí
-    console.log(response.data);
+    console.log(response.newData);
+    console.log("Se realizaron las modificaciones")
   })
   .catch(error => {
-    // Manejar errores aquí
-    console.error("Error:", error);
+    if (error.code === 'ECONNABORTED') {
+      
+      //mostrarAlerta('Servidor', {
+        //title: 'Error Personalizado',
+        //text: 'Este es un mensaje personalizado para el error de servidor.',
+      //});
+      
+      // La solicitud se canceló debido a que excedió el tiempo de espera
+      console.error("La solicitud se canceló debido a que excedió el tiempo de espera.");
+    } else {
+      // Manejar otros errores aquí
+      console.error("Error:", error);
+    }
   });
 };
 
-
-
     return (
-<div className="w-full h-screen bg-gray-100 flex flex-col justify-center items-center">
+<div className="w-full h-screen bg-gray-100  flex-col justify-center items-center">
   <h1 className="text-3xl font-bold mb-4">Personalización</h1>
 
-  <div className="mb-4">
-    <label className="text-lg font-medium">Nombre actual: {Name} </label>
-    <input
-      className="w-full border border-gray-300 rounded-xl p-3 mt-1 bg-white placeholder-gray-400 focus:outline-none focus:border-blue-500"
-      placeholder="Ingresa tu nombre"
-      value={Name}
-      onChange={(e) => setName(e.target.value)}
-    />
+  <div className="flex flex-row mb-4">
+    <p className="w-1/3 text-lg font-medium">Nombre:</p>
+    <input className="w-2/3 h-12 text-lg px-2 border border-gray-300 rounded bg-white" type="text" value={Name} readOnly={!editable} onChange={(e) => setName(e.target.value)} />
   </div>
 
-  <div className="mb-4">
-    <label className="text-lg font-medium">Apellidos: {Apellidos} </label>
-    <input
-      className="w-full border border-gray-300 rounded-xl p-3 mt-1 bg-white placeholder-gray-400 focus:outline-none focus:border-blue-500"
-      placeholder="Ingresa tu nombre"
-      value={Apellidos}
-      onChange={(e) => setApellidos(e.target.value)}
-    />
+  <div className="flex flex-row mb-4">
+    <p className="w-1/3 text-lg font-medium">Apellidos:</p>
+    <input className="w-2/3 h-12 text-lg px-2 border border-gray-300 rounded bg-white" type="text" value={Apellidos} readOnly={!editable} onChange={(e) => setApellidos(e.target.value)} />
+  </div>
+
+  <div className="flex flex-row mb-4">
+    <p className="w-1/3 text-lg font-medium">Telefono:</p>
+    <input className="w-2/3 h-12 text-lg px-2 border border-gray-300 rounded bg-white" type="text" value={Telephone} readOnly={!editable} onChange={(e) => setTelephone(e.target.value)} />
+  </div>
+
+    <h2>Cambio de contraseña</h2>
+
+
+  <div className="flex flex-row mb-4">
+    <p className="w-1/3 text-lg font-medium">Ingresa contraseña actual:</p>
+    <input className="w-2/3 h-12 text-lg px-2 border border-gray-300 rounded bg-white" type="text" value={Current} readOnly={!editable} onChange={(e) => setCurrent(e.target.value)} />
+  </div>
+
+  <div className="flex flex-row mb-4">
+    <p className="w-1/3 text-lg font-medium">Ingrese nueva contraseña: </p>
+    <input className="w-2/3 h-12 text-lg px-2 border border-gray-300 rounded bg-white" type="text" value={Password} readOnly={!editable} onChange={(e) => setPassword(e.target.value)} />
+  </div>
+
+  <div className="flex flex-row mb-4">
+    <p className="w-1/3 text-lg font-medium">confirmme su nueva contraseña: </p>
+    <input className="w-2/3 h-12 text-lg px-2 border border-gray-300 rounded bg-white" type="text" value={Confirmation} readOnly={!editable} onChange={(e) => setConfirmation(e.target.value)} />
   </div>
 
 
-  <div className="mb-4">
-    <label className="text-lg font-medium">Corregir tu teléfono</label>
-    <input
-      className="w-full border border-gray-300 rounded-xl p-3 mt-1 bg-white placeholder-gray-400 focus:outline-none focus:border-blue-500"
-      placeholder="Ingresar teléfono"
-      value={Telephone}
-      onChange={(e) => setTelephone(e.target.value)}
-    />
+  
+  <div className="flex flex-row mb-4">
+    <button
+      onClick={toggleEditable}
+      className={`py-2 px-4 rounded ${
+        editable ? 'bg-green-500 text-white' : 'bg-blue-500 text-white'
+      }`}
+    >
+      {editable ? 'Guardar' : 'Editar'}
+    </button>
+    
   </div>
 
-  <div className="mb-4">
-    <label className="text-lg font-medium">Cambio de contraseña</label>
-    <input
-      className="w-full border border-gray-300 rounded-xl p-3 mt-1 bg-white placeholder-gray-400 focus:outline-none focus:border-blue-500"
-      placeholder="Contraseña anterior"
-      type="password"
-      value={Current}
-      onChange={(e) => setCurrent(e.target.value)}
-    />
-  </div>
-
-  <div className="mb-4">
-    <label className="text-lg font-medium">Nueva contraseña</label>
-    <input
-      className="w-full border border-gray-300 rounded-xl p-3 mt-1 bg-white placeholder-gray-400 focus:outline-none focus:border-blue-500"
-      placeholder="Contraseña nueva"
-      type="password"
-      value={Password}
-      onChange={(e) => setPassword(e.target.value)}
-    />
-  </div>
-
-  <div className="mb-4">
-    <label className="text-lg font-medium">Confirmar nueva contraseña</label>
-    <input
-      className="w-full border border-gray-300 rounded-xl p-3 mt-1 bg-white placeholder-gray-400 focus:outline-none focus:border-blue-500"
-      placeholder="Confirmar contraseña"
-      type="password"
-      value={Confirmation}
-      onChange={(e) => setConfirmation(e.target.value)}
-    />
-  </div>
-  <Button onClick={ModificarClick}>Cambiar los valores del usuario</Button>
 </div>
 
     )
